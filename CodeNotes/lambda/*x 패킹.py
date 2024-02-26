db99d4af92c40fb4a1ff63782d238dbd59595e96
이경민 <124436145+2kilometer@users.noreply.github.com>
2024-02-26 20:44:55 +0900

    Create *x 패킹.py

---
+++ CodeNotes/lambda/*x 패킹.py
@@ -0,0 +1,12 @@
+solution = lambda *x:sum(x)
+
+"""
+[note]
+- 람다 -> 익명함수
+- *x -> 함수로 들어오는 인자를 튜플로 패킹
+
+-> 기존 풀이
+    def solution(num1, num2):
+        answer = num1 + num2
+        return answer
+"""
