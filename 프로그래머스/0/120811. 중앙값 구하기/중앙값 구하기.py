bca00cc66f479a1a44cb89582c3f467c1a27da8b
이경민 <124436145+2kilometer@users.noreply.github.com>
2024-02-28 15:04:36 +0900

    Update 중앙값 구하기.py

--- 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.py
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.py
@@ -1,3 +1,12 @@
 # 메모리: 10.2 MB, 시간: 0.00 ms
 def solution(array):
     return sorted(array)[len(array) // 2]
+
+# 메모리: 28.6 MB, 시간: 0.24 ms
+import numpy as np
+def solution(array):
+    return np.median(array)
+
+# 메모리: 28.8 MB, 시간: 0.17 ms
+import numpy as np
+solution = lambda array: np.median(array)
