85f0c8843ac1265a4b51b3ee5696eb970e1c7ed5
이경민 <124436145+2kilometer@users.noreply.github.com>
2024-03-03 15:08:40 +0900

    Update 특정 문자 제거하기.py

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.py
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.py
@@ -1,2 +1,7 @@
+# 메모리: 10.1 MB, 시간: 0.01 ms
 def solution(my_string, letter):
     return my_string.replace(letter, '')
+
+# 메모리: 10.2 MB, 시간: 0.01 ms
+def solution(my_string, letter):
+    return ''.join(str for str in my_string if str != letter)
