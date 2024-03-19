386d9c8be9c4fe267ab1f47bd432991bbdeff8e7
이경민 <124436145+2kilometer@users.noreply.github.com>
2024-03-19 23:31:12 +0900

    [level 3] Title: 베스트앨범, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/README.md
+++ 프로그래머스/3/42579. 베스트앨범/README.md
@@ -1,10 +1,10 @@
 # [level 3] 베스트앨범 - 42579 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 0.01 ms
+메모리: 10.1 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 19일 23:05:19
+2024년 03월 19일 23:31:11
 
 ### 문제 설명
 
--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,31 +1,20 @@
 def solution(genres, plays):
-    idx_dict = {}
-    cnt_dict = {}
-    for i, (g, p) in enumerate(zip(genres, plays)):
-        if g in idx_dict.keys():
-            if len(idx_dict[g]) < 2:                
-                if p > cnt_dict[g][0]:
-                    idx_dict[g].insert(0, i)
-                    cnt_dict[g].insert(0, p)
-                else:
-                    idx_dict[g] += [i]
-                    cnt_dict[g] += [p]
-            else:
-                if p > cnt_dict[g][0]:
-                    idx_dict[g].insert(0, i)
-                    cnt_dict[g].insert(0, p)
-                elif p > cnt_dict[g][1]:
-                    idx_dict[g].insert(1, i)
-                    cnt_dict[g].insert(1, p)
-                else:
-                    cnt_dict[g] += [p]
-                
-        else:
-            idx_dict[g] = [i]
-            cnt_dict[g] = [p]
-            
     answer = []
-    for k in sorted(cnt_dict, key=lambda x: sum(cnt_dict[x]), reverse=True):
-        answer += idx_dict[k][:2]
-        
+    dic1 = {}
+    dic2 = {}
+    for i, (g, p) in enumerate(zip(genres, plays)):
+        if g not in dic1.keys():
+            dic1[g] = [(i, p)]
+        else:
+            dic1[g] += [(i, p)]
+            
+        if g not in dic2.keys():
+            dic2[g] = p
+        else:
+            dic2[g] += p
+            
+    for k, v in sorted(dic2.items(), key=lambda x: x[1], reverse=True):
+        for i, p in sorted(dic1[k], key=lambda x: x[1], reverse=True)[:2]:
+            answer.append(i)
+    
     return answer
