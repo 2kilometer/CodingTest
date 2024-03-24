231ae18dbe768838a6282a9e3d083a505d94850b
이경민 <124436145+2kilometer@users.noreply.github.com>
2024-03-24 19:25:48 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/README.md
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/README.md
@@ -1,6 +1,6 @@
 # [level 2] 다리를 지나는 트럭 - 42583 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 24일 19:18:35
+2024년 03월 24일 19:25:46
 
 ### 문제 설명
 
--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -1,21 +1,19 @@
 from collections import deque
 def solution(bridge_length, weight, truck_weights):
-    truck_weights = deque(truck_weights)
-    next_weights = truck_weights.popleft()
+    truck = deque(truck_weights)
+    bridge = 0
+    time = 0
+    queue = deque([0] * (bridge_length))
     
-    queue = deque([0] * (bridge_length-1) + [next_weights])
-    time = 1 if truck_weights else (1+bridge_length)
-    
-    while truck_weights:
-        next_weights -= queue.popleft()
+    while truck:
+        bridge -= queue.popleft()
         
-        if (next_weights+truck_weights[0]) <= weight:
-            next_weights += truck_weights[0]
-            
-            queue.append(truck_weights.popleft())
+        if (bridge + truck[0]) <= weight:
+            bridge += truck[0]
+            queue.append(truck.popleft())
             time += 1
             
-            if not truck_weights:
+            if not truck:
                 time += bridge_length
                 break
 
