01cf8d67b4572d736f7f6eb1a61b7af1cfaf9b80
이경민 <124436145+2kilometer@users.noreply.github.com>
2024-04-05 13:31:45 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.01 ms, Memory: 28.2 MB -BaekjoonHub

--- 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.py
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.py
@@ -1,14 +1,11 @@
 import numpy as np
 def solution(keyinput, board):
     x, y = 0, 0
-    move = {'up':[0,1], 'down':[0,-1], 'left':[-1,0], 'right':[1,0]}
+    move = {'up':(0,1), 'down':(0,-1), 'left':(-1,0), 'right':(1,0)}
     
     for k in keyinput:
-        x += move[k][0]
-        y += move[k][1]
-        if any([(abs(x)>board[0]//2), (abs(y)>board[1]//2)]):
-            x -= move[k][0]
-            y -= move[k][1]
+        dx, dy = move[k][0], move[k][1]
+        if all([(abs(x+dx) <= board[0]//2), (abs(y+dy) <= board[1]//2)]):
+            x, y = x+dx, y+dy
 
-    
     return x, y
