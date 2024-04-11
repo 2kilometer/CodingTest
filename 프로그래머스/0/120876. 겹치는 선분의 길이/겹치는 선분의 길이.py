2f8f541a7c01ac2739473a962bb6317607638845
이경민 <124436145+2kilometer@users.noreply.github.com>
2024-04-11 15:30:05 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.py
+++ 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.py
@@ -1,11 +1,3 @@
 def solution(lines):
-    answer = 0
-    for i, [l1, l2] in enumerate(lines):
-        cmpr = lines[(i+1)%3]
-        answer += max(len(set(range(l1, l2+1)) & set(range(cmpr[0], cmpr[1]+1)))-1, 0)
-        
-        if i == 2:
-            cmpr2 = lines[(i+2)%3]
-            answer -= max((len(set(range(l1, l2+1)) & set(range(cmpr[0], cmpr[1]+1)) &\
-                              set(range(cmpr2[0], cmpr2[1]+1)))-1)*2, 0)
-    return answer
+    sets = [set(range(min(l), max(l))) for l in lines]
+    return len(sets[0]&sets[1] | sets[0]&sets[2] | sets[1]&sets[2])
