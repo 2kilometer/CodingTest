ace361ce586802c11adad3bda9664fccaf8decd6
Kyeongmin Lee <124436145+2kilometer@users.noreply.github.com>
2024-08-05 18:56:03 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 0.19 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
@@ -1,6 +1,14 @@
-import pandas as pd
 def solution(data, ext, val_ext, sort_by):
-    col = {"code":0, "date":1, "maximum":2, "remain":3}
-    data = pd.DataFrame(data)
-    data = data[data[col[ext]] < val_ext].sort_values(col[sort_by])
-    return data.values.tolist()
+    answer = []
+    
+    cols = ["code", "date", "maximum", "remain"]
+    ext_idx = cols.index(ext)
+    sort_idx = cols.index(sort_by)
+    
+    for d in data:
+        if d[ext_idx] < val_ext:
+            answer.append(d)
+            
+    answer.sort(key=lambda x: x[sort_idx])       
+    
+    return answer
