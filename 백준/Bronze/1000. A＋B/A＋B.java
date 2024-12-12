5ad55060feca4b386a5daebd9d38c578053bc0af
Kyeongmin Lee <124436145+2kilometer@users.noreply.github.com>
2024-12-13 01:51:52 +0900

    [Bronze V] Title: A+B, Time: 172 ms, Memory: 17780 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int value1 = scanner.nextInt();
+        int value2 = scanner.nextInt();
+        
+        System.out.println(value1 + value2);
+
+        scanner.close();
+    }
+}
