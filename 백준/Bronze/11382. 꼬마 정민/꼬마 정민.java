04222765129bcfb2452b829ebf7f63759835a5e3
Kyeongmin Lee <124436145+2kilometer@users.noreply.github.com>
2024-12-15 00:30:45 +0900

    [Bronze V] Title: 꼬마 정민, Time: 108 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        long c = Long.parseLong(st.nextToken());
+        
+        System.out.print(a + b + c);
+    }
+}
