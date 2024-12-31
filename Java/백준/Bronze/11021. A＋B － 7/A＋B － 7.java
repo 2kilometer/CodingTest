94d00eee240693a00fc7cf062235e1ed8b9112b9
Kyeongmin Lee <124436145+2kilometer@users.noreply.github.com>
2024-12-31 13:43:02 +0900

    [Bronze V] Title: A+B - 7, Time: 128 ms, Memory: 14744 KB -BaekjoonHub

---
+++ Java/백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main (String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int t = Integer.parseInt(bf.readLine());
+        
+        for (int i = 0; i < t; i++){
+            StringTokenizer st = new StringTokenizer(bf.readLine());
+            bw.write(String.format("Case #%d: %d", i+1, (Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()))) + "\n");
+        }
+        
+        bw.close();
+    }
+}
