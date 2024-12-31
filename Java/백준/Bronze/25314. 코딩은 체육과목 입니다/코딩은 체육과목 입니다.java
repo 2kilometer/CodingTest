069f5b614b051efcfb953dd7ae511d34f81e576f
Kyeongmin Lee <124436145+2kilometer@users.noreply.github.com>
2024-12-31 12:56:35 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 172 ms, Memory: 17748 KB -BaekjoonHub

---
+++ Java/백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class Main {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("long ".repeat(sc.nextInt()/4) + "int");
+    }
+}
