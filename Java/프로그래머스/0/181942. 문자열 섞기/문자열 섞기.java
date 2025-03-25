6f86eecc986eaaf1d70dab5daa9656bc79982182
Kyeongmin Lee <124436145+2kilometer@users.noreply.github.com>
2025-03-25 22:49:08 +0900

    [level 0] Title: 문자열 섞기, Time: 0.05 ms, Memory: 86 MB -BaekjoonHub

--- Java/프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
+++ Java/프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
@@ -2,8 +2,7 @@ class Solution {
     public String solution(String str1, String str2) {
         StringBuilder answer = new StringBuilder();
         for(int i = 0; i < str1.length(); i++) {
-            answer.append(str1.charAt(i));
-            answer.append(str2.charAt(i));
+            answer.append(str1.charAt(i)).append(str2.charAt(i));
         }
         return answer.toString();
     }
