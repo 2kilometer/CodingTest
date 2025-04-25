df25f0172fc36fa80ee1ea6653d12fcbe01488ab
Kyeongmin Lee <124436145+2kilometer@users.noreply.github.com>
2025-04-25 21:30:48 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.09 ms, Memory: 73.5 MB -BaekjoonHub

--- Java/프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
+++ Java/프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -1,6 +1,6 @@
 class Solution {
     public double solution(int num1, int num2) {
-        double result = Math.floor(num1 / (double)num2 * 1000);
-        return result;
+        double result = num1 / (double)num2 * 1000;
+        return (int)result;
     }
 }
